9af48b569ffeddfecbc595179d4ac2ae4b6341ee
B0Dim <bodim@me.com>
2021-12-28 12:06:39 +0300

    create actions for button and logic

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtLogin: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    private let trueLogin = "mylogin"
+    private let truePassword = "Password1"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        txtLogin.delegate = self
+        txtPassword.delegate = self
+    }
+    
+    @IBAction func actionForgotName(_ sender: UIButton) {
+        showAlert(title: "Forgot User Name", for: "Your User Name is mylogin")
+    }
+    
+    @IBAction func actionForgotPassword(_ sender: UIButton) {
+        showAlert(title: "Forgot Password", for: "Your Password is Password1")
+    }
+    
+    @IBAction func actionLogin(_ sender: UIButton) {
+        checkExistLoginAndPassword()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.placeholder == "User name" {
+            txtPassword.becomeFirstResponder()
+        } else {
+            checkExistLoginAndPassword()
+        }
 
+        return true
+    }
+
+    private func checkExistLoginAndPassword() {
+        if let login = txtLogin.text,
+           let password = txtPassword.text {
+            checkLoginAndPassword(login: login.lowercased(), and: password)
+        }
+    }
+    
+    private func checkLoginAndPassword(login: String, and password: String) {
+        if login == trueLogin && password == truePassword {
+            
+        } else {
+            showAlert(title: "Error!",
+                      for: "User name or Password is invalid.\nPlease try again.")
+        }
+    }
+    
+    private func showAlert(title: String, for text: String) {
+        let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
